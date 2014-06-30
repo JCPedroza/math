227300dd5fe9b6a14fb795c6add93a2f5c39c055
JCPedroza <nemesis4go10@hotmail.com>
2014-06-30 10:07:26 -0600

    is_prime() not uses a for loop and checks for integers

--- python/Math.py
+++ python/Math.py
@@ -15,15 +15,17 @@ def lcmm(*args):
     return reduce(lcm, args)
 
 def factorial(n):
-    """ Return factorian of n """
+    """ Return factorian of n. """
     acc = 1
     for i in range(1, n + 1): 
         acc *= i
     return acc
 
 def is_prime(n):
-    """ Is n prime? """
+    """ Check if n is a prime number. """
 
+    if n % 1 != 0:      # Integer check
+        return False
     if n < 2:
         return False
     if n in (2, 3):
@@ -31,13 +33,9 @@ def is_prime(n):
     if n % 2 == 0 or n % 3 == 0:
         return False
 
-    limit   = int(math.sqrt(n))
-    divisor = 5
-
-    while divisor <= limit:
+    for divisor in xrange(5, 1+int(math.sqrt(n)), 6):
         if n % divisor == 0 or n % (divisor + 2) == 0:
             return False
-        divisor += 6
 
     return True
 
@@ -45,6 +43,14 @@ def factors(n):
     """ Return a set of the factors of n. """
     return set(reduce(list.__add__, 
                 ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+def is_perfect_square(n):
+    """ Is n a perfect square? """
+    if n < 1:
+
+# TODO: is_perfect_square(n), is_perfect_cube(n), is_perfect_somethingelse(n)
+
+print is_prime(2.5)
 
 
 
