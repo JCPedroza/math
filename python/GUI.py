c3954dd0dcad2304f572f60ab2379e86a35189fd
JCPedroza <nemesis4go10@hotmail.com>
2014-07-08 17:52:57 -0600

    added buton for abc factors

--- python/GUI.py
+++ python/GUI.py
@@ -19,19 +19,27 @@ class Application(Frame):
 
         gcdButton = Button(self, text="gcd", command=self.gcd)
         gcdButton.grid(row=1, column=0)
+
         lcmButton = Button(self, text="lcm", command=self.lcm)
         lcmButton.grid(row=1, column=1)
+
         factorsButton = Button(self, text="factors", command=self.factors_of)
         factorsButton.grid(row=1, column=2)
+
         isprimeButton = Button(self, text="is prime?", command=self.is_prime)
         isprimeButton.grid(row=2, column=0)
+
         issquareButton = Button(self, text="is square?", command=self.is_square)
         issquareButton.grid(row=2, column=1)
+
         squarefactorsButton = Button(self, text="square factors", command=self.square_factors_of)
         squarefactorsButton.grid(row=2, column=2)
 
+        abcfactorsButton = Button(self, text="abc factors", command=self.abc_factors_of)
+        abcfactorsButton.grid(row=3, column=0)
+
         self.result = Label(self, text="Results here", justify=LEFT)
-        self.result.grid(row=3, column=0, columnspan=3)
+        self.result.grid(row=4, column=0, columnspan=3)
 
         self.pack()
         self.centerWindow()
@@ -62,7 +70,6 @@ class Application(Frame):
 
     def get_entry(self):
         """ Parses the entry input, from string to float. Ignores non-digit strings. """
-        print map(float, filter(self.is_number, self.entry.get().split()))
         return map(float, filter(self.is_number, self.entry.get().split()))
 
     def input_is_valid(self, input, min_size, max_size=10):
@@ -131,6 +138,13 @@ class Application(Frame):
         if self.input_is_valid(entry, 1):
             results = map(lambda x: sorted(Math.square_factors_of(x)), entry)
             self.result.config(text=self.build_results(entry, results))
+
+    def abc_factors_of(self):
+        """ Return factors of ac that together sum b. """
+        entry = self.get_entry()
+        if self.input_is_valid(entry, 3):
+            result = Math.abc_factors_of(*entry)
+            self.result.config(text=result)
 
 def main():
     root = Tk()
